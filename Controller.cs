5c51b46be9db74e0e7c5cc757bd14cd5324ee75a
jackorona <46652004+jackorona@users.noreply.github.com>
2020-01-15 16:48:04 -0500

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+        rb.velocity = movement * speed;
+
+      
+    }
+       
+    void Update()
+    {
+        
+    }
+}
---
+++ Objects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Objects : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+
+
+    void Start()
+    {
+        float random1 = Random.Range(-4f, 4f);
+        float random2 = Random.Range(-4f, 4f);
+        float random3 = Random.Range(-4f, 4f);
+
+        rb = GetComponent<Rigidbody>();            
+        Vector3 movement = new Vector3(random1, random2, 0);
+        rb.velocity = movement * speed;
+        rb.angularVelocity = Random.insideUnitSphere * random3;   
+    }
+
+     void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
